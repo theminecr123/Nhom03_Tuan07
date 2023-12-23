774b1d72d86e80fcfa457b85e8c0aa3b3761072b
theminecr123 <tranluudongtrieu@gmail.com>
2023-12-23 16:15:29 +0700

    fix WebView

--- app/src/main/java/com/example/nhom03_tuan07/WebViewActivity.java
+++ app/src/main/java/com/example/nhom03_tuan07/WebViewActivity.java
@@ -3,7 +3,9 @@ package com.example.nhom03_tuan07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
     private WebView webView;
@@ -18,7 +20,18 @@ public class WebViewActivity extends AppCompatActivity {
         String link = getIntent().getStringExtra("link");
         // Tải và hiển thị trang web
         webView.loadUrl(link);
+        webView.setWebViewClient(webViewClient);
+        WebSettings webSettings = webView.getSettings();
         // Bật JavaScript nếu cần
         webView.getSettings().setJavaScriptEnabled(true);
     }
+    WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // Không cho phép mở URL bằng trình duyệt mặc định
+            view.loadUrl(url);
+            return true; // WebView xử lý URL
+        }
+    };
+
 }
